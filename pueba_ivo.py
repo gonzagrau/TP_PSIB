5b963ef1d9e337a631cc17062a9ddc5dcdbea3c2
Ivo <ibajlec@itba.edu.ar>
2024-06-11 15:31:33 -0300

    correccion error

--- pueba_ivo.py
+++ pueba_ivo.py
@@ -46,6 +46,7 @@ for file in lista_paths:
         print(f"la cantidad de nans en el file {file} es {np.sum(np.isnan(trials_mean))}") """
         
         
+        trials_mean = average_EEG(trials, mode='homogenous') 
         mat_trials_mean[i,:] = trials_mean
 
         trials_amp = average_EEG(trials, mode='amp')
