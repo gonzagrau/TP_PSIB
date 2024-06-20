7fe19dde4c10333be37543460f8704fd470ad5c0
Ibajlec <ibajlec@itba.edu.ar>
2024-06-20 18:15:43 -0300

    Actualizacion funcion welch, ahora tambien hace periodograma para comparar. Falta modificar la funcion que grafica ya que ahora el imput del diccionario tiene 2 matrices y no una sola

--- TP_PSB/welch.py
+++ TP_PSB/welch.py
@@ -17,7 +17,7 @@ for file in os.listdir(directory):
         lista_paths.append(name)
         
 #genero una funcion que dado un path a un csv me devuelve la suma del welch para cada header
-def suma_welch(path,
+def suma_welch_peri(path,
                fs: int = 48000,
                plot: bool = False):
     #cargo la data
@@ -27,11 +27,13 @@ def suma_welch(path,
 
     #genero una matriz para almacenar la data
 
-    PSD_sum_mat = np.zeros((n,2))
+    PSD_sum_mat_welch = np.zeros((n,2))
+    PSD_sum_mat_Periodograma = np.zeros((n,2))
 
     for  i in range(n):
         #  Welch's 
         spl = df[headers[i]]
+        N = len(spl)
         nper = int(len(spl) // 5)
         ## se decide hacer 5 ventanas e manera empirica ya que se realizaron varios tests y es el que mas suavisa al periodograma sin tener errores por promediacion 
         ##el overlap es el clasico del 50% ya que se considera que es el que da mejores resultados
@@ -39,20 +41,35 @@ def suma_welch(path,
 
 
         suma_PSD = sum(Pxx_den)
-        PSD_sum_mat[i,0] = int(headers[i])
-        PSD_sum_mat[i,1] = suma_PSD
+        PSD_sum_mat_welch[i,0] = int(headers[i])
+        PSD_sum_mat_welch[i,1] = suma_PSD
+
+        # Periodograma
+        periodograma = np.abs(np.fft.fft(spl))**2 /N
+        # Frecs del periodograma
+        w = np.fft.fftfreq(N, 1/fs)
+
+        suma_PSD_Peri = sum(periodograma)
+        PSD_sum_mat_Periodograma[i,0] = int(headers[i])
+        PSD_sum_mat_Periodograma[i,1] = suma_PSD_Peri
+        
 
     #normalizo por el valor mas alto
-    max_per_col = np.max(PSD_sum_mat,axis=0)
-    max = max_per_col[1]
+    max_per_col_welch = np.max_welch(PSD_sum_mat_welch,axis=0)
+    max_welch = max_per_col_welch[1]
+
+    max_per_col_peri = np.max_welch(PSD_sum_mat_Periodograma)
+    max_peri = max_per_col_peri[1]
     for i in range(n):  
-        PSD_sum_mat[i,1] = PSD_sum_mat[i,1]/max
+        PSD_sum_mat_welch[i,1] = PSD_sum_mat_welch[i,1]/max_welch
+        PSD_sum_mat_Periodograma[i,1] = PSD_sum_mat_Periodograma[i,1]/max_peri
     
     #Grafica
     if plot == True:
-         graf_intensidad_vs_welch_suma(PSD_sum_mat)
+         graf_intensidad_vs_welch_suma(PSD_sum_mat_welch)
 
-    return PSD_sum_mat
+    return PSD_sum_mat_welch,PSD_sum_mat_Periodograma
+
 
 def graf_intensidad_vs_welch_suma(mat: np.array, metodo: str):
 
@@ -80,10 +97,10 @@ def graf_intensidad_vs_welch_suma(mat: np.array, metodo: str):
     plt.show()
 
 # calculo la suma del welch
-PSD_sum_mat_amp= suma_welch(lista_paths[0])
-PSD_sum_mat_both= suma_welch(lista_paths[1])
-PSD_sum_mat_homo= suma_welch(lista_paths[2])
-PSD_sum_mat_var= suma_welch(lista_paths[3])
+PSD_sum_mat_amp= suma_welch_peri(lista_paths[0])
+PSD_sum_mat_both= suma_welch_peri(lista_paths[1])
+PSD_sum_mat_homo= suma_welch_peri(lista_paths[2])
+PSD_sum_mat_var= suma_welch_peri(lista_paths[3])
 
 #almaceno las matrices 
 matrices_suma = {"amp":PSD_sum_mat_amp,"both":PSD_sum_mat_both,"homo":PSD_sum_mat_homo,"var":PSD_sum_mat_var}
@@ -95,7 +112,50 @@ for key, value in matrices_suma.items():
 
 
 
+"""
+spl_100 = df['100']
+fs = 48000
+N = len(spl_100)
 
+# Periodograma
+periodograma = np.abs(np.fft.fft(spl_100))**2 /N
+# Frecs del periodograma
+w = np.fft.fftfreq(N, 1/fs)
+#  Welch's
+nper = int(len(spl_100) // 5)
+## se decide hacer 5 ventanas e manera empirica ya que se realizaron varios tests y es el que mas suavisa al periodograma sin tener errores por promediacion
+##el overlap es el clasico del 50% ya que se considera que es el que da mejores resultados
+f, Pxx_den = welch(spl_100, fs, noverlap=nper//2, nperseg=nper) """
+
+""" # subplots
+fig, axs = plt.subplots(1, 2, figsize=(15, 5))
+
+# Plot periodograma
+axs[0].set_xlim(0,2000)
+axs[0].plot(w[:N // 2], periodograma[:N // 2])
+##coincide el largo por usar la DTFT FFT
+axs[0].set_title('Periodograma')
+axs[0].set_xlabel('Frecuencia (Hz)')
+axs[0].set_ylabel('Densidad Espectral de Potencia')
+
+axs[0].grid(True)
+
+
+# Plot Welch con 5
+axs[1].set_xlim(0,2000)
+axs[1].plot(f, Pxx_den)
+axs[1].set_title('Welch con 5')
+axs[1].set_xlabel('Frecuencia (Hz)')
+axs[1].set_ylabel('Densidad Espectral de Potencia')
+
+axs[1].grid(True)
+
+# Show
+plt.tight_layout()
+plt.show()
+
+
+ """
 
 
            
